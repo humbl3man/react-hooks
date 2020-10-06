586170a74292ae6f0c0a3278a9dc8dd8e9187bf3
Konstantin M <humbl3dev@gmail.com>
2020-10-06 16:08:07 -0700

    work on tic tac toe (ex4)

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,37 +1,60 @@
 // useState: tic tac toe
 // http://localhost:3000/isolated/exercise/04.js
 
-import React from 'react'
+import React, {useState} from 'react'
+
+function resetSquareState() {
+  return Array(9).fill(null)
+}
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = useState(resetSquareState)
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
   // - winner ('X', 'O', or null)
   // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
+
+  const [nextValue, setNextValue] = useState('X')
+  const [winner, setWinner] = useState(null)
+  const [status, setStatus] = useState(null)
+
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
+  function selectSquare(squareIndex) {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
     //
-    // 🦉 It's typically a bad idea to manipulate state in React because that
-    // can lead to subtle bugs that can easily slip into productions.
-    // 🐨 make a copy of the squares array (💰 `[...squares]` will do it!)
-    // 🐨 Set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+
+    if (winner || squares[squareIndex] !== null) {
+      return
+    }
+
+    const squaresCopy = [...squares]
+    const nextValueCopy = nextValue
+    squaresCopy[squareIndex] = nextValueCopy
+    setSquares(squaresCopy)
+    setNextValue(calculateNextValue(squaresCopy))
+    setWinner(calculateWinner(squaresCopy))
+    setStatus(
+      calculateStatus(
+        calculateWinner(squaresCopy),
+        squaresCopy,
+        calculateNextValue(squaresCopy),
+      ),
+    )
   }
 
   function restart() {
-    // 🐨 set the squares to `Array(9).fill(null)`
+    setSquares(resetSquareState)
+    setWinner(null)
+    setStatus(null)
+    setNextValue('X')
   }
 
   function renderSquare(i) {
@@ -44,8 +67,7 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status here */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
